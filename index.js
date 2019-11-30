e624ca3a5d005c35c6aa0fdd31918762b146b829
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 20:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function keepGoing(array, changeValue, skipValue){
 function findBy(array, findFn){
   for(i=0,i<array.length;i++){
     if(findFn(array[i])){
-
+      return array[i];
     }
   }
 }
