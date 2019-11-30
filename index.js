c829272802f62e7c7f183ffaf0b1037fb2ba4201
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-30 20:23:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array,changeValue,stopValue){
-  array.forEach((element,index,array){
+  array.forEach((element,index,array)=>{
     if(array[index] == stopValue){
       break;
     }
